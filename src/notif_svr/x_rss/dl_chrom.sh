24bd0c3fa3cd14f2a0f64e027e78032fcf75b07d
dmitrievichlevin <jhowar39@emich.edu>
2024-11-02 23:53:30 -0700

    XBot 0.3

--- src/notif_svr/x_rss/dl_chrom.sh
+++ src/notif_svr/x_rss/dl_chrom.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-#get latest version
-version=`curl http://chromedriver.storage.googleapis.com/LATEST_RELEASE)`;
-echo 'Currently LATEST_RELEASE:' $version;
 #download the latest version chrome driver available as per the above line
-wget -N http://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin
-chmod a+x /usr/local/bin/chromedriver
-#install latest google chrome
-yum install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-google_version=`google-chrome --version`;
-echo 'Google Chrome Version:' $google_version;
-echo 'End of the script'
+wget -N http://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d .local/bin
+chmod a+x .local/bin/chromedriver
